3d3e8736c335b8442dd54df502d3c75870ed27b8
David Tyler <davet.code@gmail.com>
2012-11-26 17:17:44 +0000

    Added angle to cartesian vector class
    Added functions to return events between two tick values to the state
    querier.

--- LibgdxCoreProject/src/net/tyler/math/CartesianVector2f.scala
+++ LibgdxCoreProject/src/net/tyler/math/CartesianVector2f.scala
@@ -2,6 +2,10 @@ package net.tyler.math
 
 import scala.math._
 
+object CartesianVectorConstants {
+  val zero = new CartesianVector2f(0f, 0f)
+}
+
 /**
  * Custom written immutable floating point vector since the Vector2f included
  * with Slick2d is mutable when using functions like scale.
@@ -16,16 +20,17 @@ case class CartesianVector2f(val x: Float, val y: Float) {
 
   def scale(factor: Float): CartesianVector2f = new CartesianVector2f(x * factor, y * factor)
 
-  def length: Float = math.sqrt(CartesianVector2f.this dot CartesianVector2f.this).toFloat
+  def length: Float = math.sqrt(this dot this).toFloat
   
   def dot(v: CartesianVector2f): Float = x * v.x + y * v.y
+  
+  def angle: Float = atan2(y, x).toFloat
   
   def rotate(angle: Float): CartesianVector2f = 
     new CartesianVector2f((x * cos(angle) - y * sin(angle)).asInstanceOf[Float], 
                           (x * sin(angle) + y * cos(angle)).asInstanceOf[Float])
   
-  def toPolar: PolarVector2f = 
-    new PolarVector2f(sqrt(x * x + y * y).toFloat, atan2(x, y).toFloat) 
+  def toPolar: PolarVector2f = new PolarVector2f(length, angle) 
 
   override def toString = "(" + x.toString + "," + y.toString + ")"
 }
